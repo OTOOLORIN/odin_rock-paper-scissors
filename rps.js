17c6f0d8e1b9d922eac31289d1713a5a688615a6
Otoolorin <volasupo5@gmail.com>
2025-02-01 12:28:22 +0100

    Restart game after a player reaches 5 points

--- rps.js
+++ rps.js
@@ -1,7 +1,3 @@
-// print welcome message to the user
-console.log("Hi there! Welcome to the game\nYou'll be playing against a computer.\nMay the gods favor you!\n\n"
-);
-
 // create storage for players' scores
 let userScore = 0;
 let computerScore  = 0;
@@ -10,9 +6,10 @@ const humanScoreBoard = document.querySelector('#human');
 const computerScoreBoard = document.querySelector('#computer')
 const humanScoreUpdate = document.createElement('span');
 const computerScoreUpdate = document.createElement('span');
+const winner = document.querySelector('#winner');
 
 humanScoreUpdate.textContent = `${userScore}`;
-computerScoreUpdate.textContent = `: ${computerScore}`;
+computerScoreUpdate.textContent = `${computerScore}`;
 
 humanScoreBoard.append(humanScoreUpdate);
 computerScoreBoard.append(computerScoreUpdate);
@@ -70,24 +67,31 @@ function playRound() {
         const computerChoice = getComputerChoice();
 
         roundResult(playerChoice, computerChoice);
+
+
+        if (userScore === 5) {
+            winner.textContent = 'You win the game!';
+        } else if (computerScore === 5) {
+            
+            winner.textContent = 'Computer win the game!';
+        }
+
+        restartGame();
      }
     
     )
 }
 
+function restartGame() {
+    if (userScore === 5 || computerScore === 5) {
+        userScore = 0;
+        computerScore = 0;
+        const restartButton = document.createElement('button');
+        restartButton.textContent = 'Restart Game';
+        winner.appendChild(restartButton);
+    }
+}
+
+
 playRound();
-
-// announce the winner
-if (userScore > computerScore) {
-    console.log("\n\nYou win!");
-} else if(userScore < computerScore) {
-    console.log("\n\nYou lose.");
-} else {
-    console.log("\n\nIt's a tie.")
-}
-// create a scoreboard
-function scoreBoard() {
-    console.log(`Human - ${userScore}        Computer - ${computerScore}`);
-}
-
 // THE END
